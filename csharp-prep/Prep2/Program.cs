042bbd57677e8c201d13ff46da7bc3fb7aefc2df
Ethan Bland <eblandisgrand@gmail.com>
2025-09-20 14:50:17 -0600

    Grade Percent

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?: ");
+
+        string StudentGrade = Console.ReadLine();
+        int number = int.Parse(StudentGrade);
+
+        string grade = "";
+
+        if (number >= 90)
+        {
+            grade = "A";
+        }
+        else if (number >= 80)
+        {
+            grade = "B";
+        }
+        else if (number >= 70)
+        {
+            grade = "C";
+        }
+        else if (number >= 60)
+        {
+            grade = "D";
+        }
+        else if (number < 60)
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {grade}");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("Good job, you passed!");
+
+        }
+        else
+        {
+            Console.WriteLine("You failed, try harder next time.");
+        }
     }
 }
