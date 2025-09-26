9db9149d4cb9ffe9b7cb4bd4d2fb0a8052998c5c
Ethan Bland <eblandisgrand@gmail.com>
2025-09-26 12:11:51 -0600

    List of Numbers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        
+        int NewNumber = -1;
+        while (NewNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            
+            string userinput = Console.ReadLine();
+            NewNumber = int.Parse(userinput);
+            
+            
+            if (NewNumber != 0)
+            {
+                numbers.Add(NewNumber);
+            }
+        }
+
+        //Compute the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        //Compute the average
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        //Compute the max
+        
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        WelcomeMessage();
+
+        string YourName = UserName();
+        int YourNumber = UserNumber();
+
+        int NumberSquared = SquareNumber(YourNumber);
+
+        int BirthYear;
+        UserBirthyear(out BirthYear);
+
+
+        DisplayResult(YourName, NumberSquared, BirthYear);
+    }
+
+    static void WelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string UserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int UserNumber()
+    {
+        Console.Write("Enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+    static void UserBirthyear(out int birthYear)
+    {
+        Console.Write("Enter the year you were born: ");
+        birthYear = int.Parse(Console.ReadLine());
+
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square, int birthYear)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}.");
+        Console.WriteLine($"{name}, you will be {2025 - birthYear} years old this year.");
     }
 }
