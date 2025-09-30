36094505017667dd66c985d91a2d42c25b315538
Ethan Bland <eblandisgrand@gmail.com>
2025-09-30 14:58:24 -0600

    Learning activity week 2. Use classes to make a resume.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Computer Engineer";
+        job1._company = "Microsoft";
+        job1._yearsOfService = "2000-2006";
+
+        Job job2 = new Job();
+        job2._jobTitle = "Elementary School Teacher";
+        job2._company = "MJUSD";
+        job2._yearsOfService = "2007-2011";
+
+        Resume myResume = new Resume();
+        myResume._name = "Ethan Bland";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
+    
 }
