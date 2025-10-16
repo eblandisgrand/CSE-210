5fd80c6047de7aeb4668030643caf396af2159d5
Ethan Bland <eblandisgrand@gmail.com>
2025-10-16 10:36:29 -0600

    Corrected semi-colon in line 10

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -7,7 +7,7 @@ class Program
 
     static void Main(string[] args)
     {
-        Console.WriteLine("hello world")
+        
         Fraction f1 = new Fraction();
         Console.WriteLine(f1.GetFractionString());
         Console.WriteLine(f1.GetDecimalValue());
