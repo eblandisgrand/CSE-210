320d079ede9ba85d90a389f8dfb0aa5f2a6be8f3
Ethan Bland <eblandisgrand@gmail.com>
2025-11-11 22:32:04 -0700

    Shape area calculator with polymorphism.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("blue", 2);
+        shapes.Add(s1);
+        Rectangle s2 = new Rectangle("red", 4, 3);
+        shapes.Add(s2);
+        Circle s3 = new Circle("purple", 6);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape's area is {area}");
+        }
     }
 }
